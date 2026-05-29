ca318e9542221a78a8ecb81c0de0224f00d2b375
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-05-29 22:45:50 +0200

    feat: add Matrix::getRows and getCols funcs

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -9,18 +9,23 @@ class Matrix {
 public:
 	Matrix(std::size_t rows, std::size_t cols) : rows{rows}, cols{cols}, data(rows * cols) {}
 
-	void scaleRow(std::size_t r, double x);
-
-	// Adds baseRow * x to targetRow.
-	void addScaledRow(std::size_t targetRow, std::size_t sourceRow, double x);
+	std::size_t getRows() const {
+		return rows;
+	}
+	std::size_t getCols() const {
+		return cols;
+	}
 
 	double& operator[](std::size_t r, std::size_t c) {
 		return data[r * cols + c];
 	}
-
 	double operator[](std::size_t r, std::size_t c) const {
 		return data[r * cols + c];
 	}
+
+	void scaleRow(std::size_t r, double x);
+	// Adds baseRow * x to targetRow.
+	void addScaledRow(std::size_t targetRow, std::size_t sourceRow, double x);
 
 private:
 	std::size_t rows = 0;
