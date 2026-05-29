f3b85525cf466e609304bede83bbfec1102d3559
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-05-29 22:00:35 +0200

    feat: create basic Matrix class

---
+++ src/math/matrix.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstddef>
+#include <vector>
+
+namespace math {
+
+class Matrix {
+public:
+	Matrix(std::size_t rows, std::size_t cols)
+		: rows{rows}, cols{cols}, data(rows * cols) {}
+
+	double& operator()(std::size_t r, std::size_t c) {
+		return data[r * cols + c];
+	}
+
+	double operator()(std::size_t r, std::size_t c) const {
+		return data[r * cols + c];
+	}
+
+private:
+	std::size_t rows = 0;
+	std::size_t cols = 0;
+	std::vector<double> data;
+};
+
+}  // namespace math
