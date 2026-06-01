46fa8a4f4abc5c26706ec36418296126d8ee89e5
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-06-01 16:47:28 +0200

    feat: implement simplex pivot method

--- src/simplex/simplex.cpp
+++ src/simplex/simplex.cpp
@@ -31,3 +31,29 @@ std::size_t Tableau::findPivotRow(std::size_t col) const {
 
 	return best;
 }
+
+void Tableau::pivot(std::size_t col, std::size_t row) {
+	// Variable at pivot column becomes basic, the one in the pivot row becomes non-basic.
+	basis[row] = col;
+
+	// Scale row such that [col, row] is 1.
+	matrix.scaleRow(row, 1.0 / matrix[col, row]);
+	rhs[row] /= matrix[col, row];
+
+	// Make other values in pivot column 0.
+	for (std::size_t r = 0; r < rhs.size(); r++) {
+		if (r == row)
+			continue;
+
+		double scale = matrix[col, r] / matrix[col, row];
+		matrix.addScaledRow(r, row, -scale);
+		rhs[r] -= rhs[row] * scale;
+	}
+
+	// Update contribution row.
+	for (std::size_t c = 0; c < contribution.size(); c++) {
+		contribution[c] = 0;
+		for (std::size_t r = 0; r < rhs.size(); r++)
+			contribution[c] += objective[basis[r]] * matrix[c, r];
+	}
+}
--- src/simplex/simplex.h
+++ src/simplex/simplex.h
@@ -7,12 +7,13 @@
 class Tableau {
 public:
 	Tableau(std::size_t cols, std::size_t rows, const std::vector<double>& objective)
-	    : objective{objective}, rhs(cols), contribution(rows), matrix{cols, rows} {}
+	    : objective{objective}, rhs(rows), contribution(cols), basis(rows), matrix{cols, rows} {}
 
 private:
 	std::vector<double> objective;
 	std::vector<double> rhs;
 	std::vector<double> contribution;
+	std::vector<std::size_t> basis;
 	math::Matrix matrix;
 
 	// @return The variable with the maximum objective - contribution. -1 if none are positive.
@@ -20,4 +21,6 @@ private:
 	// @param col The pivot column.
 	// @return The row with the minimum rhs / matrix value.
 	std::size_t findPivotRow(std::size_t col) const;
+
+	void pivot(std::size_t col, std::size_t row);
 };
