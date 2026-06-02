8b9434650d3fb274264302939acf3f6159ddff84
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-06-02 15:43:39 +0200

    refactor: clean up scale row test

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -11,18 +11,17 @@ TEST(MatrixTest, ScaleRow) {
 	    {1.5, 6.7, 4.2},
 	}};
 
+	std::vector<std::vector<double>> expected = {
+	    {2.0, 5.0, 6.0},
+	    {1.0, 2.0, 3.0},
+	    {0.75, 3.35, 2.1},
+	};
+
 	m.scaleRow(0, 2.0);
-	std::array<double, 3> expect{{2.0, 5.0, 6.0}};
-	for (std::size_t i = 0; i < m.getCols(); i++)
-		EXPECT_NEAR(expect[i], (m[0, i]), eps);
-
 	m.scaleRow(1, 1.0 / 3.0);
-	expect = {1.0, 2.0, 3.0};
-	for (std::size_t i = 0; i < m.getCols(); i++)
-		EXPECT_NEAR(expect[i], (m[1, i]), eps);
-
 	m.scaleRow(2, 0.5);
-	expect = {0.75, 3.35, 2.1};
-	for (std::size_t i = 0; i < m.getCols(); i++)
-		EXPECT_NEAR(expect[i], (m[2, i]), eps);
+
+	for (std::size_t r = 0; r < m.getRows(); r++)
+		for (std::size_t c = 0; c < m.getCols(); c++)
+			EXPECT_NEAR(expected[r][c], (m[r, c]), eps);
 }
