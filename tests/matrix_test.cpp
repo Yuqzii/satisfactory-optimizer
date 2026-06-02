e3e21719563337acf790255b1eb70d4a543502f7
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-06-02 15:49:49 +0200

    test: create test for Matrix::addScaledRow

--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -25,3 +25,25 @@ TEST(MatrixTest, ScaleRow) {
 		for (std::size_t c = 0; c < m.getCols(); c++)
 			EXPECT_NEAR(expected[r][c], (m[r, c]), eps);
 }
+
+TEST(MatrixTest, AddScaledRow) {
+	math::Matrix m{{
+	    {1.0, 2.5, 3.0},
+	    {3.0, 6.0, 9.0},
+	    {1.5, 6.7, 4.2},
+	}};
+
+	std::vector<std::vector<double>> expected = {
+	    {2.5, 5.5, 7.5},
+	    {1.5, -0.7, 4.8},
+	    {6.5, 17.7, 19.2},
+	};
+
+	m.addScaledRow(0, 1, 0.5);
+	m.addScaledRow(1, 2, -1);
+	m.addScaledRow(2, 0, 2);
+
+	for (std::size_t r = 0; r < m.getRows(); r++)
+		for (std::size_t c = 0; c < m.getCols(); c++)
+			EXPECT_NEAR(expected[r][c], (m[r, c]), eps);
+}
