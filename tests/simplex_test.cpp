6acfb25ebcccbbf6843fc80bfa86a9627a8132c1
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-06-05 11:58:52 +0200

    refactor: automatically instantiate test suite

--- tests/simplex_test.cpp
+++ tests/simplex_test.cpp
@@ -2,26 +2,72 @@
 
 #include <gtest/gtest.h>
 
+#include <optional>
+
+#include "gtest/gtest.h"
 #include "math/matrix.h"
 
 namespace simplex {
 
-class TableauTest : public ::testing::Test {};
+struct TableauPivotTestCase {
+	std::size_t expectedRow;
+	std::optional<std::size_t> expectedCol;
 
-TEST_F(TableauTest, FindPivotColumn) {
-	std::vector<double> objective = {7.0, 6.0, 0, 0};
-	math::Matrix matrix{{
-	    {0, 8.0 / 3.0, 1.0, -2.0 / 3.0},
-	    {1.0, 2.0 / 3.0, 0, 1.0 / 3.0},
-	}};
-	std::vector<double> rhs(matrix.getRows());
+	std::string name;
 
-	Tableau tableau{std::move(objective), std::move(rhs), std::move(matrix)};
-	tableau.basis[1] = 0;
-	tableau.calculateContribution();
+	std::vector<double> objective;
+	std::vector<double> rhs;
+	std::vector<std::size_t> basis;
+	math::Matrix matrix;
+};
 
-	std::size_t res = tableau.findPivotColumn().value();
-	EXPECT_EQ(res, 1);
+class TableauPivotTest : public ::testing::TestWithParam<TableauPivotTestCase> {};
+
+TEST_P(TableauPivotTest, FindPivot) {
+	const auto& testCase = GetParam();
+
+	Tableau tableau{
+	    std::move(testCase.objective),
+	    std::move(testCase.rhs),
+	    std::move(testCase.matrix),
+	};
+
+	if (!testCase.basis.empty()) {
+		tableau.basis = std::move(testCase.basis);
+		tableau.calculateContribution();
+	}
+
+	const auto col = tableau.findPivotColumn();
+	ASSERT_EQ(testCase.expectedCol.has_value(), col.has_value())
+	    << "Wrongly claims optimality / non-optimality";
+
+	if (!col.has_value()) {
+		SUCCEED();
+		return;
+	}
+
+	ASSERT_EQ(testCase.expectedCol.value(), col.value());
+
+	const std::size_t row = tableau.findPivotRow(col.value());
+	EXPECT_EQ(testCase.expectedRow, row);
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    Tableau, TableauPivotTest,
+    ::testing::Values(
+        TableauPivotTestCase{
+            .expectedRow = 1,
+            .expectedCol = 0,
+            .name = "InitialState",
+            .objective{7, 6, 0, 0},
+            .rhs{16, 12},
+            .matrix{{
+                {2, 4, 1, 0},
+                {3, 2, 0, 1},
+            }},
+        }
+    ),
+    [](const ::testing::TestParamInfo<TableauPivotTestCase>& info) { return info.param.name; }
+);
 
 }  // namespace simplex
