ce53ab05efc2bfb7075f11984b75571e5c041612
Dru C. Romano Abello <druabello@gmail.com>
2026-06-11 19:25:37 +0200

    fix: initialize constructors for RecipeItem and Recipe

--- src/recipes/recipes.h
+++ src/recipes/recipes.h
@@ -7,12 +7,17 @@
 struct RecipeItem {
 	int amount;
 	std::string name;
+
+	RecipeItem(int a, std::string n) : amount(a), name(n) {};
 };
 
 struct Recipe {
 	double duration;
 	std::vector<RecipeItem> ingredients;
 	std::vector<RecipeItem> products;
+
+	Recipe(double dur, std::vector<RecipeItem> ingr, std::vector<RecipeItem> prod)
+	    : duration(dur), ingredients(ingr), products(prod) {};
 };
 
 std::vector<Recipe> getRecipes(std::string& filePath);
