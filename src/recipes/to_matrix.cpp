12d82bb2b38f70a3d16c364c223de4bf9cb99195
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-07-04 21:36:00 +0200

    refactor: avoid intermediate 2D vector for columns
    Immediately creates a matrix of the correct size rather than first
    creating a 2D vector and then converting it to a matrix.

--- src/recipes/to_matrix.cpp
+++ src/recipes/to_matrix.cpp
@@ -24,31 +24,34 @@ std::vector<std::size_t> itemsToIdx(
 }  // namespace
 
 math::Matrix recipesToMatrix(const std::span<const Recipe> recipes, ItemManager& itemManager) {
-	std::vector<std::vector<double>> columns;
+	// Get all the indices of items first, such that we know the total amount of items.
+	std::vector<std::vector<std::size_t>> ingredientIdx, productIdx;
+	ingredientIdx.reserve(recipes.size());
+	productIdx.reserve(recipes.size());
 
 	for (const auto& recipe : recipes) {
-		// Get all the indices of items first, such that we don't need to resize the newRow vector
-		// because we already know the maximum item index.
-		const std::vector<std::size_t> ingredientIdx = itemsToIdx(recipe.ingredients, itemManager);
-		const std::vector<std::size_t> productIdx = itemsToIdx(recipe.products, itemManager);
+		auto curIngredients = itemsToIdx(recipe.ingredients, itemManager);
+		ingredientIdx.push_back(std::move(curIngredients));
 
-		std::vector<double> newCol(itemManager.size());
-		for (std::size_t i = 0; i < recipe.ingredients.size(); i++) {
-			double res = static_cast<double>(recipe.ingredients[i].amount) / recipe.duration;
-			newCol[ingredientIdx[i]] -= res;  // Negative because it consumes ingredients.
-		}
-		for (std::size_t i = 0; i < recipe.products.size(); i++) {
-			double res = static_cast<double>(recipe.products[i].amount) / recipe.duration;
-			newCol[productIdx[i]] += res;  // Positive because it produces the products.
-		}
-
-		columns.push_back(std::move(newCol));
+		auto curProducts = itemsToIdx(recipe.products, itemManager);
+		productIdx.push_back(std::move(curProducts));
 	}
 
-	math::Matrix matrix{itemManager.size(), columns.size()};
-	for (std::size_t c = 0; c < columns.size(); c++)
-		for (std::size_t r = 0; r < columns[c].size(); r++)
-			matrix[r, c] = columns[c][r];
+
+	math::Matrix matrix{itemManager.size(), recipes.size()};
+	for (std::size_t i = 0; i < recipes.size(); i++) {
+		for (std::size_t j = 0; j < recipes[i].ingredients.size(); j++) {
+			const double cur =
+			    static_cast<double>(recipes[i].ingredients[j].amount) / recipes[i].duration;
+			matrix[ingredientIdx[i][j], i] -= cur; // Negative for consumption.
+		}
+
+		for (std::size_t j = 0; j < recipes[i].products.size(); j++) {
+			const double cur =
+			    static_cast<double>(recipes[i].products[j].amount) / recipes[i].duration;
+			matrix[productIdx[i][j], i] += cur; // Positive for production.
+		}
+	}
 
 	return matrix;
 }
