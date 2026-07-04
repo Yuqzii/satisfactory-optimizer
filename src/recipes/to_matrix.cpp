c8881bf0e8c5cc0888d4e0b5aad4994f07a140c9
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-07-04 21:51:43 +0200

    refactor: avoid 2D index vector

--- src/recipes/to_matrix.cpp
+++ src/recipes/to_matrix.cpp
@@ -6,50 +6,43 @@
 
 namespace {
 
-std::vector<std::size_t> itemsToIdx(
-    const std::span<const RecipeItem> items, ItemManager& itemManager
-) {
-	std::vector<std::size_t> res;
-	res.reserve(items.size());
-
+template <std::output_iterator<std::size_t> OutputIt>
+void itemsToIdx(const std::span<const RecipeItem> items, ItemManager& itemManager, OutputIt out) {
 	// Get the item indices of all items.
-	std::transform(
-	    items.begin(), items.end(), std::back_inserter(res),
-	    [&itemManager](const RecipeItem& item) { return itemManager[item.name]; }
-	);
-
-	return res;
+	std::transform(items.begin(), items.end(), out, [&itemManager](const RecipeItem& item) {
+		return itemManager[item.name];
+	});
 }
 
 }  // namespace
 
 math::Matrix recipesToMatrix(const std::span<const Recipe> recipes, ItemManager& itemManager) {
 	// Get all the indices of items first, such that we know the total amount of items.
-	std::vector<std::vector<std::size_t>> ingredientIdx, productIdx;
-	ingredientIdx.reserve(recipes.size());
-	productIdx.reserve(recipes.size());
+	std::vector<std::size_t> ingredientIdx, ingredientOffset, productIdx, productOffset;
+	ingredientOffset.reserve(recipes.size());
+	productOffset.reserve(recipes.size());
 
 	for (const auto& recipe : recipes) {
-		auto curIngredients = itemsToIdx(recipe.ingredients, itemManager);
-		ingredientIdx.push_back(std::move(curIngredients));
-
-		auto curProducts = itemsToIdx(recipe.products, itemManager);
-		productIdx.push_back(std::move(curProducts));
+		ingredientOffset.push_back(ingredientIdx.size());
+		productOffset.push_back(productIdx.size());
+		itemsToIdx(recipe.ingredients, itemManager, std::back_inserter(ingredientIdx));
+		itemsToIdx(recipe.products, itemManager, std::back_inserter(productIdx));
 	}
-
 
 	math::Matrix matrix{itemManager.size(), recipes.size()};
 	for (std::size_t i = 0; i < recipes.size(); i++) {
 		for (std::size_t j = 0; j < recipes[i].ingredients.size(); j++) {
 			const double cur =
 			    static_cast<double>(recipes[i].ingredients[j].amount) / recipes[i].duration;
-			matrix[ingredientIdx[i][j], i] -= cur; // Negative for consumption.
+			const std::size_t idx = ingredientIdx[ingredientOffset[i] + j];
+			matrix[idx, i] -= cur;  // Negative for consumption.
 		}
 
 		for (std::size_t j = 0; j < recipes[i].products.size(); j++) {
 			const double cur =
 			    static_cast<double>(recipes[i].products[j].amount) / recipes[i].duration;
-			matrix[productIdx[i][j], i] += cur; // Positive for production.
+			const std::size_t idx = productIdx[productOffset[i] + j];
+			matrix[idx, i] += cur;  // Positive for production.
 		}
 	}
 
