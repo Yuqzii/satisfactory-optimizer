8e00230e3deb309749d673b336d2d49ccf39c26a
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-07-04 21:21:37 +0200

    fix: swap rows and columns
    Before this commit the code produced a matrix where the rows and columns
    were opposite of what the simplex algorithm expects.

--- src/recipes/to_matrix.cpp
+++ src/recipes/to_matrix.cpp
@@ -24,7 +24,7 @@ std::vector<std::size_t> itemsToIdx(
 }  // namespace
 
 math::Matrix recipesToMatrix(const std::span<const Recipe> recipes, ItemManager& itemManager) {
-	std::vector<std::vector<double>> matrix;
+	std::vector<std::vector<double>> columns;
 
 	for (const auto& recipe : recipes) {
 		// Get all the indices of items first, such that we don't need to resize the newRow vector
@@ -32,20 +32,25 @@ math::Matrix recipesToMatrix(const std::span<const Recipe> recipes, ItemManager&
 		const std::vector<std::size_t> ingredientIdx = itemsToIdx(recipe.ingredients, itemManager);
 		const std::vector<std::size_t> productIdx = itemsToIdx(recipe.products, itemManager);
 
-		std::vector<double> newRow(itemManager.size());
+		std::vector<double> newCol(itemManager.size());
 		for (std::size_t i = 0; i < recipe.ingredients.size(); i++) {
 			double res = static_cast<double>(recipe.ingredients[i].amount) / recipe.duration;
-			newRow[ingredientIdx[i]] -= res;  // Negative because it consumes ingredients.
+			newCol[ingredientIdx[i]] -= res;  // Negative because it consumes ingredients.
 		}
 		for (std::size_t i = 0; i < recipe.products.size(); i++) {
 			double res = static_cast<double>(recipe.products[i].amount) / recipe.duration;
-			newRow[productIdx[i]] += res;  // Positive because it produces the products.
+			newCol[productIdx[i]] += res;  // Positive because it produces the products.
 		}
 
-		matrix.push_back(std::move(newRow));
+		columns.push_back(std::move(newCol));
 	}
 
-	return math::Matrix{matrix};
+	math::Matrix matrix{itemManager.size(), columns.size()};
+	for (std::size_t c = 0; c < columns.size(); c++)
+		for (std::size_t r = 0; r < columns[c].size(); r++)
+			matrix[r, c] = columns[c][r];
+
+	return matrix;
 }
 
 std::size_t ItemManager::operator[](const std::string_view key) {
